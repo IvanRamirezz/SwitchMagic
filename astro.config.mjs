8d57e763f1861b2642b94c03397a53e24e30f34f
IvanRamirezz <92224804+IvanRamirezz@users.noreply.github.com>
2025-04-12 09:13:57 -0600

    Guardando cambios antes del pull

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://IvanRamirezz.github.io',
-    base: 'SwitchMagic',
-  });
+  site: 'https://IvanRamirezz.github.io',
+  base: 'SwitchMagic',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
