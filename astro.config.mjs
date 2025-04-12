51206e5bd069b8abdd5ac435023a60d62ee294d1
IvanRamirezz <92224804+IvanRamirezz@users.noreply.github.com>
2025-04-12 08:47:58 -0600

    Guardando cambios antes del pull

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    base: '/SwitchMagic/', // <- muy importante para GitHub Pages
+    site: 'https://IvanRamirezz.github.io',
+    base: 'SwitchMagic',
   });
