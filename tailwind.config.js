f984bce6d852bde5e55dc61efa20af48679a4dc7
IvanRamirezz <92224804+IvanRamirezz@users.noreply.github.com>
2025-04-13 10:28:14 -0600

    Guardando cambios antes del pull

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
+    "./src/styles/**/*.css"
+  ],
   theme: {
     extend: {},
   },
